25d4c619e03b8da21c15670e32b93e2d42f1ec3e
Mohamed Taher <mo.taher717@gmail.com>
2024-09-21 03:32:34 +0300

    update -  Implement Filtering, Searching, and Ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -13,7 +13,7 @@ class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend,filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title','author', 'publication_year']
     search_fields = ['title','author__name']
     ordering_fields = '__all__'
